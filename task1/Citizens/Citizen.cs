009c1fcb23e94d4ab049f3545e36c3afd4923102
id347627 <id347627@gmail.com>
2016-06-14 01:41:06 +0300

    Add

---
+++ task1/Citizens/Citizen.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+
+namespace Citizens
+{
+
+    public class Citizen : ICitizen
+    {
+        private Gender gender;
+        private DateTime dateOfBirth;
+        private string lastName;
+        private string firstName;
+        private string vatId;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+        }
+
+        public Gender Gender
+        {
+            get
+            {
+                return gender;
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get
+            {
+                return dateOfBirth;
+            }
+        }
+
+        public string VatId
+        {
+            get
+            {
+                return vatId;
+            }
+
+            set
+            {
+                vatId = value;
+            }
+        }
+
+
+
+        public Citizen()
+        { }
+
+        public Citizen(string firstName, string lastName, DateTime dateOfBirth, Gender gender)
+        {
+
+
+            this.firstName = firstName.Transform(To.LowerCase, To.TitleCase);
+            this.lastName = lastName.Transform(To.LowerCase, To.TitleCase);
+            if ((SystemDateTime.Now() - dateOfBirth).TotalDays >= 0)
+            {
+                this.dateOfBirth = dateOfBirth.Date;
+            }
+            else
+            {
+                throw new ArgumentException(String.Format("Person who was not born cannot be added. The passed date is {0}", dateOfBirth), "dateOfBirth");
+            }
+
+            if (Enum.IsDefined(typeof(Gender), gender))
+            {
+                this.gender = gender;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public Citizen(ICitizen ob)
+        {
+            this.firstName = ob.FirstName;
+            this.lastName = ob.LastName;
+            this.dateOfBirth = ob.BirthDate;
+            this.gender = ob.Gender;
+            this.vatId = ob.VatId;
+        }
+    }
+}
---
+++ task1/Citizens/CitizenRegister.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+using System.Globalization;
+
+namespace Citizens
+{
+    public class CitizenRegister : ICitizenRegistry
+    {
+        public ICitizen[] citizenArray = new ICitizen[20];
+        public DateTime lastRegDate;
+
+        public ICitizen this[string id]
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(id))
+                {
+                    throw new ArgumentNullException();
+                }
+
+                for (int j = 0; j < citizenArray.Length; j++)
+                {
+                    if (citizenArray[j] != null && string.Equals(citizenArray[j].VatId, id))
+                    {
+                        return citizenArray[j];
+                    }
+                }
+                return null;
+            }
+        }
+
+        public void Register(ICitizen citizen)
+        {
+            if (String.IsNullOrEmpty(citizen.VatId))
+            {
+                int sn = 0;
+                int[] mul = { -1, 5, 7, 9, 4, 6, 10, 5, 7 };
+
+                switch (citizen.Gender)
+                {
+                    case Gender.Female:
+                        {
+                            sn = 0;
+                            break;
+                        }
+                    case Gender.Male:
+                        {
+                            sn = 1;
+                            break;
+                        }
+                }
+                string dateString = ((citizen.BirthDate - new DateTime(1899, 12, 31)).TotalDays).ToString().PadLeft(5, '0');
+                string serNString;
+                string checkNumber;
+                do
+                {
+                    serNString = sn.ToString().PadLeft(4, '0');
+
+                    int sum = 0;
+                    string charArr = string.Concat(dateString, serNString);
+
+                    for (int i = 0; i < charArr.Length; i++)
+                    {
+                        sum += int.Parse(charArr[i].ToString()) * mul[i];
+                    }
+                    checkNumber = ((sum % 11) % 10).ToString();
+                    sn += 2;
+                }
+                while (this[string.Concat(dateString, serNString, checkNumber)] != null);
+
+                citizen.VatId = string.Concat(dateString, serNString, checkNumber);
+            }
+
+            if (this[citizen.VatId] == null)
+            {
+                for (int i = 0; i < citizenArray.Length; i++)
+                    if (citizenArray[i] == null)
+                    {
+                        citizenArray[i] = new Citizen(citizen);
+                        lastRegDate = SystemDateTime.Now();
+                        break;
+                    }
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+
+        }
+
+        public string Stats()
+        {
+            int maleN = 0;
+            int femaleN = 0;
+
+            foreach (ICitizen ob in citizenArray)
+            {
+                if (ob != null)
+                    if (int.Parse(ob.VatId[8].ToString()) % 2 == 0)
+                    {
+                        femaleN++;
+                    }
+                    else
+                    {
+                        maleN++;
+                    }
+            }
+            
+            string retStr = String.Join(" ", "man".ToQuantity(maleN), "and", "woman".ToQuantity(femaleN));
+
+            double diff = (lastRegDate - SystemDateTime.Now()).TotalDays;
+
+            if ((maleN > 0 || femaleN > 0) && diff < 0)
+            {
+                retStr = string.Join(". ", retStr, "Last registration was " +
+                    DateTimeOffset.Now.AddDays(diff).Humanize(culture: new CultureInfo("en-US")));
+            }
+            return retStr;
+        }
+    }
+}
